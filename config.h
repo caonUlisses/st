21c913905e367cd8943eeac8c3eedfd26beb64c8
Ulisses Caon <ulissescaon@gmail.com>
2021-07-18 17:45:36 -0300

    Enable font ligatures

--- config.h
+++ config.h
@@ -5,7 +5,7 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "Hack Nerd Font:pixelsize=11:antialias=true:autohint=true";
+static char *font = "FiraCode Nerd Font:pixelsize=11:antialias=true:autohint=true";
 static char *font2[] = { "JoyPixels:pixelsize=11:antialias=true:autohint=true" };
 static int borderpx = 2;
 
